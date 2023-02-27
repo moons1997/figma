7269f921773bab14eff83237328e13490c327779
moons1997 <umar1997mengliyev@gmail.com>
2023-02-28 01:09:01 +0500

    last

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="c-wrapper">
+    <header></header>
+    <main>
+      <div class="c-container">
+        <div class="sidebar"></div>
+        <div class="main"></div>
+      </div>
+    </main>
+    <footer></footer>
+  </div>
+
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="aaaaaaa">
+
+    <RouterView />
+  </div> -->
+</template>
+
+<style scoped></style>
